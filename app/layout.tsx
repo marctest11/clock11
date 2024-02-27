c4ae6c2e040457feb6abdff8466121901d4a2d10
marcramate <161449399+marctest11@users.noreply.github.com>
2024-02-27 19:10:39 +0700

    #2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import BgMain from "@/component/bgmain";
+import BgMain from "@/components/bgmain";
 
 const inter = Inter({ subsets: ["latin"] });
 
